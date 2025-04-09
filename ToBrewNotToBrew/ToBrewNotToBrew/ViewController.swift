8f82b52c31ca74c2fe091d187d605b6eb1754e02
kkt6394 <kkt6394@gmail.com>
2025-04-09 11:25:31 +0900

    feat: 테이블 뷰 구성

--- ToBrewNotToBrew/ToBrewNotToBrew/ViewController.swift
+++ ToBrewNotToBrew/ToBrewNotToBrew/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let orderTableView = TableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         // Do any additional setup after loading the view.
+    }
+    private func setupUI() {
+        view.backgroundColor = .white
+        view.addSubview(orderTableView)
+        
+        orderTableView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
     }
 
 
--- ToBrewNotToBrew/ToBrewNotToBrew/orderDetails.swift
+++ ToBrewNotToBrew/ToBrewNotToBrew/orderDetails.swift
@@ -8,6 +8,53 @@
 import UIKit
 import SnapKit
 
+
+class TableView: UIView {
+    
+    let tableView = UITableView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupTableView()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // 셀 등록
+        tableView.register(OrderItemCell.self, forCellReuseIdentifier: "OrderItemCell")
+        // 테이블 뷰제약 조건
+        tableView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.bottom.equalToSuperview()
+        }
+        
+    }
+}
+extension TableView: UITableViewDelegate, UITableViewDataSource {
+    // 테이블 뷰 행 개수. 1. 장바구니에 아무것도 없을 때, 2. 장바구니에 담겨있을 때로 구성해야함.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    // 테이블 뷰 내용.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "OrderItemCell", for: indexPath) as? OrderItemCell else {
+            return UITableViewCell()
+        }
+        cell.configure()
+        return cell
+    }
+    
+    
+}
+
+// 테이블 뷰 셀
 class OrderItemCell: UITableViewCell {
     let statusLabel = UILabel()
     let cartImageView = UIImageView()
@@ -32,10 +79,9 @@ class OrderItemCell: UITableViewCell {
             $0.width.equalTo(32)
             $0.top.equalToSuperview().offset(55)
             $0.bottom.equalToSuperview().offset(81)
+            $0.centerX.equalToSuperview()
         }
         
-        statusLabel.text = "장바구니가 비어있습니다."
-        statusLabel.font = .systemFont(ofSize: 14)
         
         statusLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
@@ -44,6 +90,7 @@ class OrderItemCell: UITableViewCell {
         }
     }
     func configure() {
-        
+        statusLabel.text = "장바구니가 비어있습니다."
+        cartImageView.image = UIImage(named: "shoppingCart")
     }
 }
